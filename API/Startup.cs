1ca5cf9488323770fab2be7c3ba707d945242b86
El Mustapha Lagzouli <emlagzouli@sqli.com>
2021-10-01 11:29:08 +0100

    End of Section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,22 +25,25 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
+
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddApplicationServices();
             services.AddSwaggerDocumention();
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy => 
-                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
-                );
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
@@ -53,9 +56,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
-            
-            app.UseCors();
 
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from 'src/models/pagination';
+import { IProduct } from 'src/models/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[]=[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<IPagination>("https://localhost:44363/api/products?pageSize=50").subscribe((response: IPagination) => {
+      this.products=response.data;
+      console.log(response)
+    },error =>{
+      console.log(error)
+    })
+  }
+
+
 }
